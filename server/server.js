c35a8235c617efecc519524ca4ee3cf340f19450
Mohsin <mohammadmohsin@gmail.com>
2018-02-08 12:31:32 +0500

    Some test changes

--- server/server.js
+++ server/server.js
@@ -11,19 +11,8 @@ app.listen(3000, () => {
 });
 
 
-//*** ONLY for testing purposes */
-app.get("/get_weather", (req, res) => {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
-
-
-  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response 
-  //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
-});
-
 app.post("/get_weather", (req, res) => {
-  response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+  response = "This is a sample response from your webhook Anas!" //Default response from the webhook to show it's working
 
 
   res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
