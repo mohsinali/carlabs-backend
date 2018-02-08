53141609b6f4cec105922470c52f291bd6217d3e
Mohsin <mohammadmohsin@gmail.com>
2018-02-08 14:36:30 +0500

    Some code refactoring. Using environment variables for private data.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 // var { mongoose } = require('./db/mongoose');
+var { get_weather } = require('./controllers/get_weahter');
 
 var app = express();
 app.use(bodyParser.json());
@@ -11,15 +12,8 @@ app.listen(3000, () => {
 });
 
 
-app.post("/get_weather", (req, res) => {
-  response = "This is a sample response from your webhook Anas!" //Default response from the webhook to show it's working
 
-
-  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
-  res.send(JSON.stringify({ "speech": response, "displayText": response 
-  //"speech" is the spoken version of the response, "displayText" is the visual version
-  }));
-});
+app.post("/get_weather", get_weather);
 
 
 module.exports = { app };
